4fe87af14e61e6c9212d6c52cf6533ea3a3bd0ca
OscarGarciaPeinado <garcia95721@gmail.com>
2014-02-09 01:01:45 +0100

    Implementada vista de arhivos, todos los archivos son mostrados en el fragment adecuado, la funcionalidad de buscar archivos en todas las carpetas de dropbox también funciona bien.

--- AppBQ/src/com/oscar/garcia/appBQ/entities/Book.java
+++ AppBQ/src/com/oscar/garcia/appBQ/entities/Book.java
@@ -1,17 +1,92 @@
 package com.oscar.garcia.appBQ.entities;
 
+import java.io.InputStream;
 import java.util.Date;
 
-public class Book {
+import nl.siegmann.epublib.epub.EpubReader;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.dropbox.client2.DropboxAPI.Entry;
+import com.dropbox.client2.RESTUtility;
+import com.oscar.garcia.appBQ.assests.Constants;
+
+public class Book implements Comparable {
 	private String _tittle;
 	private Date _date;
-	private String _path;
+	private String _dropBoxPath;
+	private String _internalPath;
+	private boolean _downloaded;
+	private String _fileName;
+	private Bitmap _coverImage;
 
-	public Book(String _tittle, Date _date, String _path) {
+	public Book(String _tittle, Date _date, String _path, String _fileName) {
 		super();
 		this._tittle = _tittle;
 		this._date = _date;
-		this._path = _path;
+		this._dropBoxPath = _path;
+		this._fileName = _fileName;
+		_downloaded = false;
+	}
+
+	public Book(Entry entry) {
+		// TODO: TIENES QUE PARSEAR UN ENTRY A UN BOOK, IMPORTANTE!
+		_date = RESTUtility.parseDate(entry.modified);
+		_fileName = entry.fileName();
+		_dropBoxPath = entry.path;
+	}
+
+	public void parserBook(AssetManager assetManager) {
+		try {
+			InputStream epubInputStream = assetManager.open(_internalPath + "/"
+					+ _fileName);
+			nl.siegmann.epublib.domain.Book book = (new EpubReader())
+					.readEpub(epubInputStream);
+			_tittle = book.getMetadata().getTitles().toString();
+			_coverImage = BitmapFactory.decodeStream(book.getCoverImage()
+					.getInputStream());
+		} catch (Exception e) {
+	
+		}
+	}
+
+	/**
+	 * @param _downloaded
+	 *            the _downloaded to set
+	 */
+	public void set_downloaded(boolean _downloaded) {
+		this._downloaded = _downloaded;
+	}
+
+	/**
+	 * @return the _internalPath
+	 */
+	public String get_internalPath() {
+		return _internalPath;
+	}
+
+	/**
+	 * @return the _coverImage
+	 */
+	public Bitmap get_coverImage() {
+		return _coverImage;
+	}
+
+	/**
+	 * @return the _fileName
+	 */
+	public String get_fileName() {
+		return _fileName;
+	}
+
+	/**
+	 * @param _fileName
+	 *            the _fileName to set
+	 */
+	public void set_fileName(String _fileName) {
+		this._fileName = _fileName;
 	}
 
 	/**
@@ -47,16 +122,39 @@ public class Book {
 	/**
 	 * @return the _path
 	 */
-	public String get_path() {
-		return _path;
+	public String get_dropBoxPath() {
+		return _dropBoxPath;
 	}
 
 	/**
-	 * @param _path
-	 *            the _path to set
+	 * @return the _downloaded
 	 */
-	public void set_path(String _path) {
-		this._path = _path;
+	public boolean is_downloaded() {
+		return _downloaded;
 	}
 
+	@Override
+	public int compareTo(Object another) {
+		int aux = 0;
+		switch (Constants.getTYPE_COMPARATOR()) {
+		case 1:
+			aux = _fileName.compareTo(((Book) another).get_fileName());
+			break;
+		case 2:
+			aux = _date.compareTo(((Book) another).get_date());
+			break;
+		default:
+			try {
+				Log.e(Constants.TAG_ERROR,
+						"La constante de comparación no coincide con ningún tipo de oradenación.");
+				throw new Exception(
+						"La constante de comparación no coincide con ningún tipo de oradenación.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+
+		return aux;
+	}
 }
